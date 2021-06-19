f508b255cd5467fb56a7c8563204b2b6297942e2
rohit <rajeshbhatrohit@gmail.com>
2021-06-19 14:22:08 +0800

    updated formatting

--- models/user.go
+++ models/user.go
@@ -1,18 +1,18 @@
 package models
 
 type User struct {
-	ID                string `json:"user_id"`
-	Name              string `json:"name"`
-	Email             string `json:"email"`
-	Password          string `json:"password"`
-	DateOfBirth       string  `json:"date_of_birth"`
-	Gender            string `json:"gender"`
-	Age               int16  `json:"age"`
-	Availability 	[]string `json:"availability"`
-	SMSNotification   bool   `json:"sms_notification"`
-	EmailNotification bool   `json:"email_notification"`
-	CreatedAt         string  `json:"created_at"`
-	UpdatedAt         string  `json:"updated_at"`
+	ID                string   `json:"user_id"`
+	Name              string   `json:"name"`
+	Email             string   `json:"email"`
+	Password          string   `json:"password"`
+	DateOfBirth       string   `json:"date_of_birth"`
+	Gender            string   `json:"gender"`
+	Age               int16    `json:"age"`
+	Availability      []string `json:"availability"`
+	SMSNotification   bool     `json:"sms_notification"`
+	EmailNotification bool     `json:"email_notification"`
+	CreatedAt         string   `json:"created_at"`
+	UpdatedAt         string   `json:"updated_at"`
 }
 
 func (u User) Signup() *User {
