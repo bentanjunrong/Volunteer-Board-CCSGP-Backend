bfd6a113effa0b0edcedbcbc75d6b0f755a7fb20
rohit <rajeshbhatrohit@gmail.com>
2021-06-21 01:30:57 +0800

    updated models with create method

--- models/user.go
+++ models/user.go
@@ -1,13 +1,22 @@
 package models
 
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"strings"
+
+	"github.com/bentanjunrong/Volunteer-Board-CCSGP-Backend/db"
+	"github.com/elastic/go-elasticsearch/v7/esapi"
+)
+
 type User struct {
-	ID                string   `json:"user_id"`
-	Name              string   `json:"name"`
-	Email             string   `json:"email"`
-	Password          string   `json:"password"`
-	DateOfBirth       string   `json:"date_of_birth"`
-	Gender            string   `json:"gender"`
-	Age               int16    `json:"age"`
+	Name              string   `json:"name" binding:"required"`
+	Email             string   `json:"email" binding:"required"`
+	Password          string   `json:"password" binding:"required"`
+	DateOfBirth       string   `json:"date_of_birth" binding:"required"`
+	Gender            string   `json:"gender" binding:"required"`
+	Age               int16    `json:"age" binding:"required"`
 	Availability      []string `json:"availability"`
 	SMSNotification   bool     `json:"sms_notification"`
 	EmailNotification bool     `json:"email_notification"`
@@ -15,6 +24,31 @@ type User struct {
 	UpdatedAt         string   `json:"updated_at"`
 }
 
-func (u User) Signup() *User {
-	return &u
+func (u *User) Create(user User) error {
+	// Build the request body.
+	var b strings.Builder
+	b.WriteString(`{"title" : "users"}`)
+
+	// Set up the request object.
+	body, err := json.Marshal(user)
+	req := esapi.IndexRequest{
+		Index: "users",
+		Body:  strings.NewReader(string(body)),
+	}
+
+	// Perform the request with the client.
+	res, err := req.Do(context.Background(), db.GetDB())
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	res.Body.Close()
+	return nil
+}
+
+func (u *User) Update(user User) (*User, error) {
+	return u, nil
+}
+
+func (u *User) Delete(user User) (*User, error) {
+	return u, nil
 }
