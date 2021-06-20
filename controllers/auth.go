2411f66be3e7be5c5b45321aa30c1b61aa1279a6
rohit <rajeshbhatrohit@gmail.com>
2021-06-21 01:32:20 +0800

    updated auth controller to use model create method

--- controllers/auth.go
+++ controllers/auth.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/bentanjunrong/Volunteer-Board-CCSGP-Backend/models"
 	"github.com/gin-gonic/gin"
 )
@@ -9,6 +11,21 @@ type AuthController struct{}
 
 var authModel = new(models.User)
 
-func (a *AuthController) Register(ctx *gin.Context) {}
+func (a *AuthController) Register(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
 
-func (a *AuthController) Login(ctx *gin.Context) {}
+	if err := authModel.Create(user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"user": user})
+}
