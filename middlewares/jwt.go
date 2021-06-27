9c187c078a9075078fc0feb06db6fbee373eee6b
rohit <rajeshbhatrohit@gmail.com>
2021-06-27 19:07:52 +0800

    updated middleware return type

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -9,36 +9,29 @@ import (
 )
 
 // expects "Authorization" header for any requests that require auth with value "Bearer <JWT Token>".
-func Auth() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		clientToken := c.GetHeader("Authorization")
-		if clientToken == "" {
-			c.AbortWithStatus(http.StatusUnauthorized)
-			return
-		}
-
-		extractedToken := strings.Split(clientToken, "Bearer ")
-
-		if len(extractedToken) <= 1 {
-			c.JSON(http.StatusBadRequest, "Incorrect Format of Authorization Token.")
-			c.Abort()
-			return
-		}
-
-		jwtWrapper := utils.JwtWrapper{
-			SecretKey: "mySecretKey",
-			Issuer:    "VolunteeryAuthService",
-		}
-
-		claims, err := jwtWrapper.ValidateToken(strings.TrimSpace(extractedToken[1]))
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, err.Error())
-			c.Abort()
-			return
-		}
-
-		c.Set("userID", claims.UserID)
-
-		c.Next()
+func Auth(c *gin.Context) {
+	clientToken := c.GetHeader("Authorization")
+	if clientToken == "" {
+		c.AbortWithStatus(http.StatusUnauthorized)
 	}
+
+	extractedToken := strings.Split(clientToken, "Bearer ")
+	if len(extractedToken) <= 1 {
+		c.JSON(http.StatusBadRequest, "Incorrect Format of Authorization Token.")
+		c.Abort()
+	}
+
+	jwtWrapper := utils.JwtWrapper{
+		SecretKey: "mySecretKey",
+		Issuer:    "VolunteeryAuthService",
+	}
+
+	claims, err := jwtWrapper.ValidateToken(strings.TrimSpace(extractedToken[1]))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, err.Error())
+		c.Abort()
+	}
+
+	c.Set("userID", claims.UserID)
+	c.Next()
 }
