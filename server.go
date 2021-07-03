9b881fc52df48c211cd8e5f21b96da66ebeceb7c
rohit <rajeshbhatrohit@gmail.com>
2021-07-03 16:52:14 +0800

    added dotenv

--- server.go
+++ server.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/bentanjunrong/Volunteer-Board-CCSGP-Backend/db"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	db.InitMongoDB()
 	db.InitDB()
 	InitRouter()
