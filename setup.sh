108436f1958b7e27da3a5574325e4e8b3c3a3f25
rohit <rajeshbhatrohit@gmail.com>
2021-07-04 17:23:47 +0800

    updated sed string delimiters and monstache service config

--- setup.sh
+++ setup.sh
@@ -7,8 +7,11 @@ grep -v '^#' .env
 export $(grep -v '^#' .env | xargs)
 
 # Update vals in monstache.toml
-sed -i s/DB_URL/$DB_URL/g monstache.toml
-sed -i s/ES_PASSWORD/$ES_PASSWORD/g monstache.toml
+sed -i "s|DB_URL|$DB_URL|g" monstache.toml
+sed -i "s|ES_PASSWORD|$ES_PASSWORD|g" monstache.toml
+
+# Copy monstache.toml to systemd directory
+sudo cp monstache.toml /etc/systemd/system/monstache.service.d/monstache.toml
 
 {
     echo "[Service]"; 
