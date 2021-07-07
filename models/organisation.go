82761da72fc9b7d8e43084c76904c43ac38177ec
rohit <rajeshbhatrohit@gmail.com>
2021-07-07 22:19:30 +0800

    cleaned up org and user model

--- models/organisation.go
+++ models/organisation.go
@@ -1,16 +1,5 @@
 package models
 
-import (
-	"context"
-	"encoding/json"
-	"log"
-	"strings"
-
-	"github.com/bentanjunrong/Volunteer-Board-CCSGP-Backend/db"
-	"github.com/bentanjunrong/Volunteer-Board-CCSGP-Backend/utils"
-	"github.com/elastic/go-elasticsearch/v7/esapi"
-)
-
 type Organisation struct {
 	Name          string        `json:"name" binding:"required"`
 	Email         string        `json:"email" binding:"required"`
@@ -22,32 +11,4 @@ type Organisation struct {
 	Opportunities []Opportunity `json:"opportunities"`
 	CreatedAt     string        `json:"created_at"`
 	UpdatedAt     string        `json:"updated_at"`
-}
-
-func (o *Organisation) Create(org Organisation) (Organisation, error) {
-	// Set up the request object.
-	org.Password = utils.Hash(org.Password)
-	body, err := json.Marshal(org)
-	if err != nil {
-		log.Fatalf("Error marshalling organisation: %s", err)
-	}
-	req := esapi.IndexRequest{
-		Index: "orgs",
-		Body:  strings.NewReader(string(body)),
-	}
-
-	// Perform the request with the client.
-	res, err := req.Do(context.Background(), db.GetDB())
-	res.Body.Close()
-	return org, err
-}
-
-func (o *Organisation) Read(email string) (map[string]interface{}, error) {
-	org, err := db.GetOneByField("orgs", map[string]string{
-		"email": email,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return org, nil
 }
--- models/user.go
+++ models/user.go
@@ -1,15 +1,5 @@
 package models
 
-import (
-	"context"
-	"encoding/json"
-	"log"
-	"strings"
-
-	"github.com/bentanjunrong/Volunteer-Board-CCSGP-Backend/utils"
-	"github.com/elastic/go-elasticsearch/v7/esapi"
-)
-
 type AcceptedOpportunity struct {
 	OppID    string   `json:"opp_id" bson:"opp_id" binding:"required"`
 	ShiftIDs []string `json:"shift_ids" bson:"shift_ids" binding:"required"`
@@ -23,39 +13,11 @@ type User struct {
 	Gender                string                `json:"gender" bson:"gender"`
 	Age                   int16                 `json:"age" bson:"age"`
 	Availability          []string              `json:"availability" bson:"availability"`
-	AcceptedOpportunities []AcceptedOpportunity `json:"accepted_opportunities" bson:"accepted_opportunities"`
+	AcceptedOpportunities []AcceptedOpportunity `json:"accepted_opps" bson:"accepted_opps"`
 	SMSNotification       bool                  `json:"sms_notification" bson:"sms_notification"`
 	EmailNotification     bool                  `json:"email_notification" bson:"email_notification"`
 	CreatedAt             string                `json:"created_at" bson:"created_at"`
 	UpdatedAt             string                `json:"updated_at" bson:"updated_at"`
-}
-
-func (u *User) Create(user User) (User, error) {
-	// Set up the request object.
-	user.Password = utils.Hash(user.Password)
-	body, err := json.Marshal(user)
-	if err != nil {
-		log.Fatalf("Error marshalling user: %s", err)
-	}
-	req := esapi.IndexRequest{
-		Index: "users",
-		Body:  strings.NewReader(string(body)),
-	}
-
-	// Perform the request with the client.
-	res, err := req.Do(context.Background(), db.GetDB())
-	res.Body.Close()
-	return user, err
-}
-
-func (u *User) Read(email string) (map[string]interface{}, error) {
-	user, err := db.GetOneByField("users", map[string]string{
-		"email": email,
-	})
-	if err != nil {
-		return nil, err
-	}
-	return user, nil
 }
 
 func (u *User) Update(user User) (*User, error) {
